4081c6e93c7ef5708018f91d18889b3293aadf67
Nikolaos Ktistakis <nktistakis@crowdpolicy.com>
2022-09-22 18:07:31 +0300

    s

--- src/main/java/gr/ds/restapi/config/WebSecurityConfig.java
+++ src/main/java/gr/ds/restapi/config/WebSecurityConfig.java
@@ -95,7 +95,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOriginPatterns(Collections.singletonList("http://20.224.66.212/"));
+        config.setAllowedOriginPatterns(Collections.singletonList("http://20.224.66.212"));
         config.setAllowedHeaders(Arrays.asList("Authorization","Origin", "Content-Type", "Accept", "X-Custom-Header", "X-Requested-With"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
         source.registerCorsConfiguration("/**", config);
