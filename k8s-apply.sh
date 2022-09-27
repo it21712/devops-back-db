52008f8bc77b57657688ecac6812e79f1549c283
brightdean <konstantinos.vroustouris@gmail.com>
2022-09-27 14:13:23 +0000

    added https support, updated react-ingress.yaml, updated k8s-apply.sh to create tls-secret and apply react-ingress.yaml

--- k8s-apply.sh
+++ k8s-apply.sh
@@ -6,4 +6,9 @@ sudo microk8s kubectl apply -f k8s/spring/spring-config.yaml && \
 sudo microk8s kubectl apply -f k8s/spring/spring-deployment.yaml && \
 sudo microk8s kubectl apply -f k8s/spring/spring-clip.yaml && \
 sudo microk8s kubectl apply -f k8s/react/react-deployment.yaml && \
-sudo microk8s kubectl apply -f k8s/react/react-clip.yaml
+sudo microk8s kubectl apply -f k8s/react/react-clip.yaml && \
+sudo microk8s kubectl create secret generic tls-secret \
+--from-file=tls.crt=certs/certificate.crt \
+--from-file=tls.key=certs/private.key \
+--from-file=ca.crt=certs/ca_bundle.crt && \
+sudo microk8s kubectl apply -f k8s/react/react-ingress.yaml
