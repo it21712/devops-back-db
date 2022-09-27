aaf2b6e499f504363a2a01b3697b88b6aefcd7db
brightdean <konstantinos.vroustouris@gmail.com>
2022-09-27 15:45:50 +0000

    added certs/react and certs/spring, spring-ingress.yaml

--- k8s-apply.sh
+++ k8s-apply.sh
@@ -7,8 +7,14 @@ sudo microk8s kubectl apply -f k8s/spring/spring-deployment.yaml && \
 sudo microk8s kubectl apply -f k8s/spring/spring-clip.yaml && \
 sudo microk8s kubectl apply -f k8s/react/react-deployment.yaml && \
 sudo microk8s kubectl apply -f k8s/react/react-clip.yaml && \
-sudo microk8s kubectl create secret generic tls-secret \
---from-file=tls.crt=certs/certificate.crt \
---from-file=tls.key=certs/private.key \
---from-file=ca.crt=certs/ca_bundle.crt && \
-sudo microk8s kubectl apply -f k8s/react/react-ingress.yaml
+sudo microk8s kubectl create secret generic react-tls-secret \
+--from-file=tls.crt=certs/react/certificate.crt \
+--from-file=tls.key=certs/react/private.key \
+--from-file=ca.crt=certs/react/ca_bundle.crt && \
+sudo microk8s kubectl apply -f k8s/react/react-ingress.yaml && \
+sudo microk8s kubectl create secret generic spring-tls-secret \
+--from-file=tls.crt=certs/spring/certificate.crt \
+--from-file=tls.key=certs/spring/private.key \
+--from-file=ca.crt=certs/spring/ca_bundle.crt && \
+sudo microk8s kubectl apply -f k8s/spring/spring-ingress.yaml
+
---
+++ k8s-stop.sh
@@ -0,0 +1,2 @@
+sudo microk8s kubectl delete deploy mysql react spring && \
+sudo microk8s kubectl delete ing react-ingress spring-ingress
