5eaf369f617debc73668b10e66c9b95ac642c10e
Ubuntu <azureuser@devopsmachine.wxmur0rkjbdurfwj1dys0kugde.ax.internal.cloudapp.net>
2022-09-28 19:43:08 +0000

    updated fqdn

--- k8s-stop.sh
+++ k8s-stop.sh
@@ -1,2 +1,4 @@
-sudo microk8s kubectl delete deploy mysql react spring && \
+#!/bin/bash
+sudo microk8s kubectl delete deploy mysql react spring
 sudo microk8s kubectl delete ing react-ingress spring-ingress
+sudo microk8s kubectl delete secret react-tls-secret spring-tls-secret
